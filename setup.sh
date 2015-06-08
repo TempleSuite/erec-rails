2ca0ddadb9569642863acfbc09c19e7c0fc492e2
Katelyn Sievert <katelyn.sievert@gmail.com>
2015-06-08 14:40:59 -0700

    fix some bundler issues

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 bash docker-compose.sh build
-bash docker-compose.sh run web bundle --path /bundle/ruby-2.2.1
+bash docker-compose.sh run web bundle install --path /bundle/ruby-2.2.1
 bash docker-compose.sh run web rake db:create
 bash docker-compose.sh run web rake db:migrate
