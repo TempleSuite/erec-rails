7dadfc892c0163eddc2f690c31eea211439e33a5
Katelyn Sievert <katelyn.sievert@gmail.com>
2015-06-11 14:32:21 -0700

    build docker-compose when runnign setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+docker build -t erec/docker-compose docker/docker-compose/
 bash docker-compose.sh build
 bash docker-compose.sh run --rm web bundle install
 bash docker-compose.sh run --rm web bundle exec rake db:create
