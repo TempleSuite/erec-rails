91cf0bfbdd319f29ca40d548c8fc3f7e42eeaab5
Katelyn Sievert <katelyn.sievert@gmail.com>
2015-06-11 12:38:39 -0700

    General Public can create a user account
    https://www.pivotaltracker.com/story/show/91619342
    
    This allows the general public to create a user account for themselves at `/users/new`. Also includes an integration test for valid parameters.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
   end
 
@@ -12,5 +30,17 @@ class UsersController < ApplicationController
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :gender,
+      :email,
+      :born_at,
+      :password,
+      :password_confirmation
+    )
   end
 end
