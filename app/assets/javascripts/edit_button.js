be09a797ec7d6e2f86e84a9c4efd308bd1aba7a6
TheTurtleMoves <TheTurtleMoves@gmail.com>
2015-06-15 14:17:39 -0700

    CR Fixes

--- app/assets/javascripts/edit_button.js
+++ app/assets/javascripts/edit_button.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-  $('.form-edit-button').click(function(editButton) {
-    editButton.preventDefault();
+  $('.form-edit-button').click(function(event) {
+    event.preventDefault();
     // toggle visibility of hidden button
     $('[data-row=' + $(this).data("form") + ']').toggleClass('hidden');
   });
 
   /* Update Widget Button - hides itself on click */
-  $('.update-button').on('click', function(updateButton) {
-    updateButton.preventDefault();
+  $('.update-button').on('click', function(event) {
+    event.preventDefault();
     hideButton($(this));
   });
 
   /* Hide the clicked Update Widget Button */
-  hideButton = function(updateButton) {
-    $('[data-row=' + updateButton.data("update") + ']').toggleClass('hidden');
+  hideButton = function(buttonElement) {
+    $('[data-row=' + buttonElement.data("update") + ']').toggleClass('hidden');
   }
 });
